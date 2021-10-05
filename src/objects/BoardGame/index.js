b6136dbf0fb0a3575710694ec27f6cb3fad4e46d
Matheussgsilva <matheussgsilva@hotmail.com>
2021-10-05 15:33:15 -0300

    Add CardFrontBack in BoardGame

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,9 +1,10 @@
+import CardFrontBack from "../../components/CardFrontBack";
 import "./style.css";
-import CardGame from "../../components/CardGame";
+
 
 function BoardGame(amountCard) {
-    const $htmlCardGame = CardGame()
-    const $htmlContent = $htmlCardGame.repeat(amountCard)
+    const $htmlCardFrontBack = CardFrontBack()
+    const $htmlContent = $htmlCardFrontBack.repeat(amountCard)
 
     return `
         <section class="board-game">
