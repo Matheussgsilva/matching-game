2132324799412e513a16c9a3c289c60ad71490b7
Matheussgsilva <matheussgsilva@hotmail.com>
2021-10-06 19:12:09 -0300

    Create PlayerScore and Arrow Down

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,15 @@
+import "./style.css"
+
+function PlayerScore() {
+    return /*html*/`
+        <table class="player-score">
+            <tr>
+                <td class="one">&nbsp;</td>
+                <td class="two">&nbsp;</td>
+                <td class="three">&nbsp;</td>
+            </tr>
+        </table>
+    `
+}
+
+export default PlayerScore
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,16 @@
 import PlayerId from "../../components/PlayerId";
+import PlayerScore from "../../components/PlayerScore";
 import "./style.css";
 
 function ScoreBoard() {
     return /*html*/`
+        <div class="arrow-down"><img src="../../images/IconArrowDown.png"></div>
         <header class="score-board">
             ${PlayerId("Player1")}
+            ${PlayerScore()}
+            <p>vs</p>
             ${PlayerId("Player2")}
+            ${PlayerScore()}
         </header>
     `
 }
